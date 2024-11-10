a0d6b84e3b15c87759f42b350818a4311e79c8eb
Gustavo Ramos <gustavo.ramos7049@gmail.com>
2024-11-09 23:29:30 -0300

    Correções nos arquivos

--- src/main/java/com/improveit/ImproveIt/repositories/IdeiaRepository.java
+++ src/main/java/com/improveit/ImproveIt/repositories/IdeiaRepository.java
@@ -1,6 +1,6 @@
 package com.improveit.ImproveIt.repositories;
 
-import com.improveit.ImproveIt.domain.user.Ideia;
+import com.improveit.ImproveIt.domain.ideia.Ideia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
