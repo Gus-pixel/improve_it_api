b38953c5144c81114bc1a9e74898f9693b42e9b1
Gustavo Ramos <gustavo.ramos7049@gmail.com>
2024-11-09 21:43:30 -0300

    Configurando MC de usuários

--- src/main/java/com/improveit/ImproveIt/repositories/UsuarioRepository.java
+++ src/main/java/com/improveit/ImproveIt/repositories/UsuarioRepository.java
@@ -1,10 +1,8 @@
 package com.improveit.ImproveIt.repositories;
 
-import com.improveit.ImproveIt.domain.user.Usuario;
+import com.improveit.ImproveIt.domain.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
-
-}
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {}
--- src/main/resources/db/migration/V1__create-user-table.sql
+++ src/main/resources/db/migration/V1__create-user-table.sql
@@ -2,11 +2,11 @@
 
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE TABLE IF NOT EXISTS "user" (
+CREATE TABLE IF NOT EXISTS "usuario" (
     id UUID gen_random_uuid() PRIMARY KEY,
     username VARCHAR(20) NOT NULL,
     senha VARCHAR(20) NOT NULL,
-    nome VARCHAR(20),
-    email VARCHAR(50) NOT NULL,
+    nome VARCHAR(50),
+    cargo BOOLEAN NOT NULL,
     status BOOLEAN NOT NULL
 );
