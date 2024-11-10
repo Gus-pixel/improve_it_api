a84b1ef29aa847d22951251897da3eafb5186091
main <giovanegambaro95@gmail.com>
2024-11-09 23:18:05 -0300

    Revert "Descrição do Commit:"
    This reverts commit 7bc273d89761ccebb35b4faeafa1c3620dc16870.

--- src/main/java/com/improveit/ImproveIt/domain/user/Usuario.java
+++ src/main/java/com/improveit/ImproveIt/domain/user/Usuario.java
@@ -28,6 +28,3 @@ public class Usuario {
     private  String email;
     private  Boolean status;
 }
-
-
-
--- src/main/java/com/improveit/ImproveIt/repositories/PilarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.improveit.ImproveIt.repositories;
-
-import com.improveit.ImproveIt.domain.user.Pilar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface PilarRepository extends JpaRepository< Pilar, UUID> {
-}
