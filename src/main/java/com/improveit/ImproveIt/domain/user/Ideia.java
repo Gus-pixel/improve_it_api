a16f44d855df3b81c66462a7ab74644dc20fa0b1
Kaike Maiante <kaikemaiante@gmail.com>
2024-11-09 23:13:25 -0300

    Criacao da classe 'Ideia'

---
+++ src/main/java/com/improveit/ImproveIt/domain/user/Ideia.java
@@ -0,0 +1,33 @@
+package com.improveit.ImproveIt.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "ideia")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Ideia {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String titulo;
+    private String desc_ideia;
+    private String desc_problema;
+    private Date data_ideia;
+    private Boolean status;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+}
