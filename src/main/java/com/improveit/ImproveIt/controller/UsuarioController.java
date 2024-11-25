a6be9081500d6f6062aee9e4ac6d2d572660eb8a
Gustavo Ramos <gustavo.ramos7049@gmail.com>
2024-11-25 06:51:52 -0300

    feat: criando cruds para a entidade usuário

--- src/main/java/com/improveit/ImproveIt/controller/UsuarioController.java
+++ src/main/java/com/improveit/ImproveIt/controller/UsuarioController.java
@@ -5,10 +5,10 @@ import com.improveit.ImproveIt.domain.usuario.UsuarioRequestDTO;
 import com.improveit.ImproveIt.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuario")
@@ -21,5 +21,23 @@ public class UsuarioController {
     public ResponseEntity<Usuario> create(@RequestBody UsuarioRequestDTO body){
         Usuario newUsuario = this.usuarioService.createUsuario(body);
         return ResponseEntity.ok(newUsuario);
+    }
+
+    @PutMapping("/{uuid}")
+    public ResponseEntity<Usuario> updateUsuario(@PathVariable UUID uuid, @RequestBody UsuarioRequestDTO body) {
+        Usuario updatedUsuario = this.usuarioService.updateUsuario(uuid, body);
+        return ResponseEntity.ok(updatedUsuario);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getUsuarios() {
+        List<Usuario> usuarios = this.usuarioService.getUsuarios();
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<Usuario> getUsuario(@PathVariable UUID uuid) {
+        Usuario usuario = this.usuarioService.getUsuarioById(uuid);
+        return ResponseEntity.ok(usuario);
     }
 }
